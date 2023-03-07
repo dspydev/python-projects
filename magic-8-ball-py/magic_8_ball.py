dccacc9c2abe34984b717cad6431e3e2f37c9ddb
Damien SOULÉ <115745200+dspydev@users.noreply.github.com>
2023-03-07 15:48:31 +0400

    Update magic_8_ball.py with Chat GPT API

--- magic-8-ball-py/magic_8_ball.py
+++ magic-8-ball-py/magic_8_ball.py
@@ -1,4 +1,32 @@
+import openai
 import random
+
+# Set up OpenAI API credentials
+openai.api_key = "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"
+
+# Set up GPT-3 parameters
+model_engine = "text-davinci-002"
+prompt_prefix = "Q: "
+temperature = 0.5
+max_tokens = 100
+
+def generate_response(prompt):
+    # Generate response using GPT-3
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt_prefix + prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract the response text from the API result
+    response_text = response.choices[0].text.strip()
+
+    return response_text
 
 def main():
     try:
@@ -13,23 +41,8 @@ def main():
             if not user_question:
                 raise EOFError
 
-            # List of possible answers
-            possible_answers = [
-                "Yes",
-                "No",
-                "Maybe",
-                "It is certain",
-                "Very doubtful",
-                "Ask again later",
-                "Cannot predict now",
-                "Outlook good"
-            ]
-
-            # Generate random index
-            random_index = random.randint(0, len(possible_answers) - 1)
-
-            # Assign answer based on random_index
-            selected_answer = possible_answers[random_index]
+            # Generate response
+            response = generate_response(user_question)
 
             # Print output
             if user_name:
@@ -40,7 +53,7 @@ def main():
             if not user_question:
                 print("Please ask a question!")
             else:
-                print("Magic 8-Ball's answer: " + selected_answer)
+                print("Magic 8-Ball's answer: " + response)
 
             # Ask user if they want to play again
             while True:
